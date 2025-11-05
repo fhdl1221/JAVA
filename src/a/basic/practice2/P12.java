095ff770920bd827d63358c7b2314e2ddc0a7ed2
fhdl1221 <fhdl353212@gmail.com>
2025-11-05 11:40:56 +0900

    코드 수정

--- src/a/basic/practice2/P12.java
+++ src/a/basic/practice2/P12.java
@@ -3,15 +3,19 @@ package a.basic.practice2;
 public class P12 {
     public static void main(String[] args) {
         int num = 12345;
-        int[] arr = new int[5];
-        int i = 0;
+//        int[] arr = new int[5];
+//        int i = 0;
+//
+//        while(num > 0) {
+//            arr[i] = num % 10;
+//            num /= 10;
+//            System.out.print(arr[i]);
+//            i++;
+//        }
 
-        while(num > 0) {
-            arr[i] = num % 10;
-            num /= 10;
-            System.out.print(arr[i]);
-            i++;
+        String str = Integer.toString(num);
+        for(int i = str.length() - 1; i >= 0; i--){
+            System.out.print(str.charAt(i));
         }
-
     }
 }
