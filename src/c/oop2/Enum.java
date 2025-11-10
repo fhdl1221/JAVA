60a429afa7da9b75e2c48c5cd958a2926dbceaf6
fhdl1221 <fhdl353212@gmail.com>
2025-11-11 08:56:13 +0900

    java oop2

---
+++ src/c/oop2/Enum.java
@@ -0,0 +1,16 @@
+package c.oop2;
+
+public class Enum {
+    public static void main(String[] args) {
+        Direction direc = Direction.NORTH;
+        System.out.println(direc);
+
+        System.out.println(direc.name());
+        System.out.println(direc.ordinal());
+
+        Direction[] directions = Direction.values();
+        for(Direction d : directions) {
+            System.out.println(d.name());
+        }
+    }
+}
