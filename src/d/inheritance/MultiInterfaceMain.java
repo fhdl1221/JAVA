ae76b37ff05153ed98af1f69f1eab4f0d18a798f
fhdl1221 <fhdl353212@gmail.com>
2025-11-11 14:15:47 +0900

    java interface

---
+++ src/d/inheritance/MultiInterfaceMain.java
@@ -0,0 +1,40 @@
+package d.inheritance;
+
+interface Swimmable {
+    void swim();
+}
+
+interface Flyable {
+    void fly();
+}
+
+interface Walkable {
+    void walk();
+}
+
+class Duck implements Swimmable, Flyable, Walkable {
+    @Override
+    public void fly() {
+        System.out.println("오리가 날아갑니다.");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("오리가 수영합니다.");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("오리가 걸어갑니다.");
+    }
+}
+
+class Fish implements Swimmable {
+    @Override
+    public void swim() {
+        System.out.println("물고기가 수영합니다.");
+    }
+}
+public class MultiInterfaceMain {
+
+}
